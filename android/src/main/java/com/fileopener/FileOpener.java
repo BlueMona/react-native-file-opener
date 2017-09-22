9ffcb70a5028ed2fabd4ffcb68bda9fdba4d5d96
Samvel Avanesov <seavan@gmail.com>
2017-09-22 14:32:03 -0400

    feat: allow opening of content:// urls

--- android/src/main/java/com/fileopener/FileOpener.java
+++ android/src/main/java/com/fileopener/FileOpener.java
@@ -1,65 +1,59 @@
 package com.fileopener;
 
-import java.io.File;
-
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import java.util.Map;
+import org.json.JSONException;
+
+import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
 
 public class FileOpener extends ReactContextBaseJavaModule {
 
-  public FileOpener(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+    public FileOpener(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
 
-  @Override
-  public String getName() {
-    return "FileOpener";
-  }
+    @Override
+    public String getName() {
+        return "FileOpener";
+    }
 
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    return constants;
-  }
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        return constants;
+    }
 
-  @ReactMethod
-  public void open(String fileArg, String contentType, Promise promise) throws JSONException {
-  		File file = new File(fileArg);
-
-  		if (file.exists()) {
-  			try {
-  				Uri path = Uri.fromFile(file);
-  				Intent intent = new Intent(Intent.ACTION_VIEW);
-  				intent.setDataAndType(path, contentType);
-  				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-  				getReactApplicationContext().startActivity(intent);
-
-                promise.resolve("Open success!!");
-  			} catch (android.content.ActivityNotFoundException e) {
-                promise.reject("Open error!!");
-  			}
-  		} else {
-            promise.reject("File not found");
-  		}
-  	}
-
+    @ReactMethod
+    public void open(String fileArg, String contentType, Promise promise) throws JSONException {
+        Uri path = null;
+        if (fileArg.startsWith("content:")) {
+            path = Uri.parse(fileArg);
+        } else {
+            File file = new File(fileArg);
+            if (file.exists()) {
+                path = Uri.fromFile(file);
+            } else {
+                promise.reject("FileOpener.java: File not found");
+                return;
+            }
+        }
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(path, contentType);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getReactApplicationContext().startActivity(intent);
+            promise.resolve("FileOpener.java: open success");
+        } catch (android.content.ActivityNotFoundException e) {
+            promise.reject("FileOpener.java: open error - activity not found");
+        }
+    }
 }
