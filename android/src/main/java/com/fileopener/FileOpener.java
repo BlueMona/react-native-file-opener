52a0ae7a64db9912d69619ca89940c19e99df7b8
Samvel Avanesov <seavan@gmail.com>
2018-01-25 16:37:38 -0500

    feat: better support for different content providers [ch1425]

--- android/src/main/java/com/fileopener/FileOpener.java
+++ android/src/main/java/com/fileopener/FileOpener.java
@@ -1,7 +1,14 @@
 package com.fileopener;
 
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -31,12 +38,91 @@ public class FileOpener extends ReactContextBaseJavaModule {
         return constants;
     }
 
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = { column };
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
     @ReactMethod
     public void open(String fileArg, String contentType, String title, Promise promise) throws JSONException {
         // title is only used by iOS currently
         Uri path = null;
         if (fileArg.startsWith("content:")) {
             path = Uri.parse(fileArg);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                // DownloadsProvider
+                if (isDownloadsDocument(path)) {
+                    final String id = DocumentsContract.getDocumentId(path);
+                    path = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                }
+                // MediaProvider
+                else if (isMediaDocument(path)) {
+                    final String docId = DocumentsContract.getDocumentId(path);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+                    Uri contentUri = null;
+                    if ("image".equals(type)) {
+                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("video".equals(type)) {
+                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("audio".equals(type)) {
+                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    }
+                    final String selection = "_id=?";
+                    final String[] selectionArgs = new String[]{split[1]};
+                    final String result = getDataColumn(getReactApplicationContext(), contentUri, selection, selectionArgs);
+                    path = Uri.fromFile(new File(result));
+                }
+                // ExternalStorageProvider
+                else if (isExternalStorageDocument(path)) {
+                    final String docId = DocumentsContract.getDocumentId(path);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+                    if ("primary".equalsIgnoreCase(type)) {
+                        path = Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/" + split[1]));
+                    }
+                }
+            }
         } else {
             File file = new File(fileArg);
             if (file.exists()) {
